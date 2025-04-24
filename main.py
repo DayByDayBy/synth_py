52af57c448bf479c39b1dcab57e97c0c64a0e64a
Diomedes Git <diomedesgit@gmail.com>
2025-04-24 23:50:02 +0100

    adding linear interpolation, moved gain ramp back outside function so it's not a ramp

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import numpy as np
 import scipy.io.wavfile as wav
 
+
+def linear_interpolation(wave_table, idx):
+    truncated_idx = int(np.floor(idx))
+    next_idx = (truncated_idx+1) % wave_table.shape[0]
+    
+    next_idx_weight = idx - truncated_idx
+    truncated_idx_weight = 1 - next_idx_weight
+    
+    return truncated_idx_weight * wave_table[truncated_idx] + next_idx_weight * wave_table[next_idx]
+
+
 def main():
     sample_rate = 44100
     f = 432
@@ -19,11 +30,17 @@ def main():
     idxIncr = f * wavetable_length/sample_rate
     
     for n in range(output.shape[0]):
-        output[n] = wave_table[int(np.floor(idx))]
+        # output[n] = wave_table[int(np.floor(idx))]
+        output[n] = linear_interpolation(wave_table, idx) 
         idx += idxIncr
         idx %= wavetable_length
+        gain = -20/(n+1)-n/2
+        amplitude = 10 ** (gain/20)
+        output *= amplitude
     
-    wav.write('al_sine432hz.wav', sample_rate, output.astype(np.float32))
+    
+    
+    wav.write('sine432hz_interp.wav', sample_rate, output.astype(np.float32))
     
     
 if __name__ == "__main__":
